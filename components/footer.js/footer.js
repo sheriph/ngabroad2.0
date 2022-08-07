8efe000be6feec76cd95be9a7382c0c22406304e
sheriph <sheriph4real@gmail.com>
2022-08-07 18:34:30 +0100

    07/08/2022 v2

--- components/footer.js/footer.js
+++ components/footer.js/footer.js
@@ -1,17 +1,18 @@
+import React from "react";
 import { Box, Stack } from "@mui/material";
 import Image from "next/image";
 
 export default function Footer() {
   return (
     <Stack>
-      <Box>
+      {/* <Box>
         <Image
           src="/images/base/footerdesktop1080.jpeg"
           alt="desktop footer"
           width="100%"
           height="80%"
         />
-      </Box>
+      </Box> */}
     </Stack>
   );
 }
--- components/singlepostcard.js
+++ components/singlepostcard.js
@@ -38,7 +38,63 @@ export default function SinglePostCard({ post }) {
           <Typography>
             In today's pattern recognition class my professor talked about PCA,
             eigenvectors and eigenvalues. I understood the mathematics of it. If
-            I'm asked to find eigenvalues etc. I'll do it correctly like ...
+            I'm asked to find eigenvalues etc. I'll do it correctly like ... I
+            am an instructor in a STEM field. I am teaching an upper-division
+            course where I try to interact with the students rather than just
+            lecturing. I am teaching in the students' native language; there do
+            not seem to be any language issues. I am struggling because the
+            students tend to give wrong answers to simple questions I ask during
+            lectures. These really are simple questions; they should have
+            learned this material during the first few weeks of their first
+            year. To make matters clear, let me give an example. If we are in
+            the middle of a proof, I might ask "what is the result of log (a *
+            b)?". The right answer is "log(a) + log(b)", however, they will say
+            "log(a) * log (b)" as an answer. Then, I did not see any other way
+            and would say "no guys, it is log(a)+log(b)." Situations like this
+            repeated over the entire semester. Students complained to my boss
+            and in the student evaluation that I was demeaning them and that I
+            was upset when they answered something other than the answer I
+            wanted to. I will teach some of these students next year in another
+            course and I have a hard time trying to find a way to solve this
+            issue. The only solution I see for this is to just lecture and not
+            encourage participation in class. However, I was wondering if there
+            would be any other wiser solution. Tips from here are good, but
+            instead of a colleague, I'm dealing with students. Edit: This is
+            actually an upper-division chemistry course. The situation above
+            arises, for example, when I have to explain why pH + pOH = 14.
+            Students should have learned that in general chemistry, but I like
+            to derive it to remind them. When I perform the derivation, I start
+            from the auto-ionization of water and eventually arrive at: 14 =
+            -log ([H3O+][OH-]). Then I ask them how to simplify this in order to
+            complete the proof. But they do not remember the properties of logs.
+            I am an instructor in a STEM field. I am teaching an upper-division
+            course where I try to interact with the students rather than just
+            lecturing. I am teaching in the students' native language; there do
+            not seem to be any language issues. I am struggling because the
+            students tend to give wrong answers to simple questions I ask during
+            lectures. These really are simple questions; they should have
+            learned this material during the first few weeks of their first
+            year. To make matters clear, let me give an example. If we are in
+            the middle of a proof, I might ask "what is the result of log (a *
+            b)?". The right answer is "log(a) + log(b)", however, they will say
+            "log(a) * log (b)" as an answer. Then, I did not see any other way
+            and would say "no guys, it is log(a)+log(b)." Situations like this
+            repeated over the entire semester. Students complained to my boss
+            and in the student evaluation that I was demeaning them and that I
+            was upset when they answered something other than the answer I
+            wanted to. I will teach some of these students next year in another
+            course and I have a hard time trying to find a way to solve this
+            issue. The only solution I see for this is to just lecture and not
+            encourage participation in class. However, I was wondering if there
+            would be any other wiser solution. Tips from here are good, but
+            instead of a colleague, I'm dealing with students. Edit: This is
+            actually an upper-division chemistry course. The situation above
+            arises, for example, when I have to explain why pH + pOH = 14.
+            Students should have learned that in general chemistry, but I like
+            to derive it to remind them. When I perform the derivation, I start
+            from the auto-ionization of water and eventually arrive at: 14 =
+            -log ([H3O+][OH-]). Then I ask them how to simplify this in order to
+            complete the proof. But they do not remember the properties of logs.
           </Typography>
           <Stack spacing={1} sx={{ mt: 2 }}>
             <Stack
