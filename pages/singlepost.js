a7113d068fb5eac7cc31643921af62fa1658ed43
sheriph <sheriph4real@gmail.com>
2022-08-12 23:52:43 +0100

    12/08/2022 v1

--- pages/singlepost.js
+++ pages/singlepost.js
@@ -1,40 +1,34 @@
 import React from "react";
-import { Box, Container, Fab, Snackbar, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Fab,
+  Snackbar,
+  SpeedDial,
+  SpeedDialAction,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Footer from "../components/footer.js/footer";
 import Header from "../components/header/header";
-import SinglePostComponent from "../components/others/singlepostcomponent";
-import EditIcon from "@mui/icons-material/Edit";
+import MobileFab from "../components/others/mobilefab";
+import dynamic from "next/dynamic";
+
+const SinglePostComponent = dynamic(
+  () => import("../components/others/singlepostcomponent"),
+  { ssr: false }
+);
 
 export default function Questions() {
   return (
     <Stack spacing={1}>
       <Box component={Container}>
         <Header />
+        {/* // 
+        @ts-ignore */}
         <SinglePostComponent />
-        <Snackbar
-          open={true}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "right",
-          }}
-          autoHideDuration={null}
-        >
-          <Box
-            sx={{
-              "& > :not(style)": { m: 1 },
-              display: { xs: "flex", md: "none" },
-            }}
-          >
-            <Fab
-              size="small"
-              variant="circular"
-              color="primary"
-              aria-label="edit"
-            >
-              <EditIcon />
-            </Fab>
-          </Box>
-        </Snackbar>
+        <MobileFab />
       </Box>
       <Footer />
     </Stack>
