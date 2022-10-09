4d3864ecf4f06cad4df0d993572a9ea2da0baed3
sheriph <sheriph4real@gmail.com>
2022-10-09 22:45:29 +0100

    9/10/2022 v2

---
+++ components/flight/airportname.js
@@ -0,0 +1,51 @@
+import { Skeleton, Typography, useMediaQuery } from "@mui/material";
+import axios from "axios";
+import { get, truncate } from "lodash";
+import React from "react";
+import useSWRImmutable from "swr/immutable";
+import { titleCase } from "../../lib/utility";
+
+const getLocationName = async (iataCode) => {
+  try {
+    const name = await axios.post("/api/flights/locationname", {
+      iataCode,
+    });
+
+    return get(name, "data.data", null);
+  } catch (error) {
+    console.log("error", error);
+    throw new Error(error);
+  }
+};
+
+export default function LocationName({ iataCode, isAirport }) {
+  const { data, isLoading } = useSWRImmutable(iataCode, getLocationName, {
+    keepPreviousData: true,
+    
+  });
+
+  const mobile = useMediaQuery("(max-width:900px)", { noSsr: true });
+
+  console.log("data, iataCode", data, iataCode, isLoading);
+
+  if (isLoading) return <Skeleton width="150px" />;
+
+  return (
+    <React.Fragment>
+      {isAirport ? (
+        <Typography component="span" variant="caption">
+          {truncate(
+            `${titleCase(get(data, "name", ""))}, ${titleCase(
+              get(data, "address.cityName", "")
+            )} (${get(data, "iataCode", "")})`,
+            { length: mobile ? 45 : 500 }
+          )}
+        </Typography>
+      ) : (
+        <Typography component="span" variant="caption">{`${titleCase(
+          get(data, "address.cityName", "")
+        )}`}</Typography>
+      )}
+    </React.Fragment>
+  );
+}
