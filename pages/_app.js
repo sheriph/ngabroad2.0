d796e242f4a0096a553fb00bdc793161d3ec2393
sheriph <sheriph4real@gmail.com>
2022-10-15 23:31:27 +0100

    15/10/2022 v6

--- pages/_app.js
+++ pages/_app.js
@@ -95,7 +95,7 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <ToastContainer />
         <CssBaseline />
-          <SWRConfig value={{ provider: localStorageProvider }}>
+        {/*  <SWRConfig value={{ provider: localStorageProvider }}> */}
         <RecoilRoot>
           <Authenticator.Provider>
             <Layout>
@@ -103,7 +103,7 @@ export default function MyApp(props) {
             </Layout>
           </Authenticator.Provider>
         </RecoilRoot>
-         </SWRConfig>
+        {/*  </SWRConfig> */}
       </ThemeProvider>
     </CacheProvider>
   );
