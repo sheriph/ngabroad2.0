7517fbfa4e3d5628e28904939a330787676dff9b
sheriph <sheriph4real@gmail.com>
2022-11-09 23:25:53 +0100

    9/11/2022 V7

--- components/others/blockingloading.js
+++ components/others/blockingloading.js
@@ -1,19 +1,22 @@
 import { Backdrop } from "@mui/material";
+import Image from "next/image";
 import React from "react";
 import { BounceLoader } from "react-spinners";
+import { styled, alpha } from "@mui/material/styles";
 
 export default function BlockingLoading({ isAnimating }) {
   return (
     <Backdrop
       sx={{
         // color: "#fff",
-        zIndex: 100000000000,
-        backgroundColor: "transparent",
-        opacity: 1,
+        zIndex: (t) => t.zIndex.drawer + 100,
+        backgroundColor: (t) => alpha(t.palette.common.black, 0.5),
+        // opacity: 0.8,
       }}
       open={isAnimating}
     >
-      <BounceLoader color="#5348dc" />
+      {/* <BounceLoader color="#5348dc" /> */}
+      <Image  src="/images/base/ngaloading2.gif" width={160} height={120} />
     </Backdrop>
   );
 }
